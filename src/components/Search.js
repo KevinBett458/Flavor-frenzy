759f883af03aec8127c30c07f3298efc9c6cc113
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-05-03 16:26:48 +0300

    Completed search results feature

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,39 +2,54 @@ import React, { useState } from 'react';
 import './Search.css';
 import SearchResults from './SearchResults';
 import { SearchSharp } from '@mui/icons-material';
+import ViewRecipe from './ViewRecipe';
 
-function Search({ placeholder, data }) {
+function Search({ placeholder }) {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredData, setFilteredData] = useState([]);
+  const [selectedMealId, setSelectedMealId] = useState(null);
 
-  const handleSearch = (event) => {
-    const value = event.target.value.toLowerCase();
-    setSearchTerm(value);
-    const result = data.filter((meal) => meal.strMeal.toLowerCase().includes(value));
-    setFilteredData(result);
+  const handleSearch = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchTerm}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFilteredData(data.meals || []);
+      })
+  }
+
+  const handleViewRecipe = (Instructions) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${Instructions}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSelectedMealId(data.meals[0]);
+      });
   };
+  
 
   const handleClick = (id) => {
     // Redirect to meal page with the meal id
-    window.location.href = `/meal/${id}`;
+    setSelectedMealId(id);
   };
 
-  const handleSearchIconClick = () => {
-    const result = data.filter((meal) => meal.strMeal.toLowerCase().includes(searchTerm));
-    setFilteredData(result);
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
   }
 
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} onChange={handleSearch} />
-        <div className="searchIcon" onClick={handleSearchIconClick}>
-          <SearchSharp />
-        </div>
+        <input type="text" placeholder={placeholder} value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} onKeyDown={handleKeyDown} />
+        <button onClick={handleSearch}><SearchSharp /></button>
       </div>
+      {selectedMealId ? (
+        <ViewRecipe mealId={selectedMealId} setSelectedMealId={setSelectedMealId} handleViewRecipe={handleViewRecipe}/>
+      ) : (
       <div className='searchResultsContainer'>
         <SearchResults filteredData={filteredData} handleClick={handleClick} searchTerm={searchTerm} />
       </div>
+      )}
     </div>
   );
 }
