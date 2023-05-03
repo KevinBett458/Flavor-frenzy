ea39526f97b3ac0aa3d5640b2ee8f221a3a78f94
Yazeed <dee3ayahmed25@gmail.com>
2023-05-03 10:35:16 +0300

    adding handleAddrecipe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from './components/Search'
 import MealData from './database/db.json'
+import RecipeForm from './components/AddRecipe'
 
 function App() {
+
+  const [meals , setMeals] = useState (MealData);
+
+  const handleAddRecipe = (recipe) => {
+    setMeals ([...meals ,recipe]);
+  };
+
   return (
     <div className='App'>
       <Search placeholder='Enter a meal name....' data={MealData}/>
---
+++ src/components/AddRecipe.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function RecipeForm({ addRecipe }) {
+  const [mealName, setMealName] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [instructions, setInstructions] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addRecipe({ mealName, ingredients, instructions });
+    setMealName("");
+    setIngredients("");
+    setInstructions("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="mealName">Meal Name:</label>
+      <input
+        type="text"
+        id="mealName"
+        name="mealName"
+        value={mealName}
+        onChange={(e) => setMealName(e.target.value)}
+      /><br/><br/>
+
+      <label htmlFor="ingredients">Ingredients:</label>
+      <textarea
+        id="ingredients"
+        name="ingredients"
+        value={ingredients}
+        onChange={(e) => setIngredients(e.target.value)}
+      /><br/><br/>
+
+      <label htmlFor="instructions">Instructions:</label>
+      <textarea
+        id="instructions"
+        name="instructions"
+        value={instructions}
+        onChange={(e) => setInstructions(e.target.value)}
+      /><br/><br/>
+
+      <button type="submit">Add Recipe</button>
+    </form>
+  );
+}
+
+export default RecipeForm;
