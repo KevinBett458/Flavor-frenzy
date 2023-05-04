9cfe2010a0c6b42a4fe14808c935d6c6218a88b2
MPChege <mark.wanyoike@strathmore.edu>
2023-05-04 15:00:34 +0300

    Fixing error for fourite

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import {useContext} from 'react';
+import { FavoritesContext } from './FavoritesContext';
 
-function Favourites({ favourites }) {
+function Favourites() {
+  const { favorites, setFavorites } = useContext(FavoritesContext);
+
+  const addToFavorites = (meal) => {
+    setFavorites([...favorites, meal]);
+  };
+  
   return (
-    <div className='favourites'>
-      <h3>Favourites</h3>
-      <ul>
-        {favourites.map((favourite) => (
-          <li key={favourite.idMeal}>{favourite.strMeal}</li>
+    <div className='favorites'>
+      <ul addToFavorites={addToFavorites}>
+        {favorites.map((favorite) => (
+          <li key={favorite.idMeal}>{favorite.strMeal}</li>
         ))}
       </ul>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
-import React from "react";
+import { useContext } from "react";
 import { Link } from 'react-router-dom';
+import FavoritesContext from "./FavoritesContext";
 
 function Navbar() {
+    const { favorites } = useContext(FavoritesContext);
     return (
         <nav>
             <div>
                 <Link to="/">Home</Link>
-                <Link to="/favourites">Favorites</Link>
+                <Link to="/favorites">Favourites ({favorites.length})</Link>
                 <Link to="/about">About</Link>
 
             </div>
