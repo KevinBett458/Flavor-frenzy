332ceb0938aa75e5c1449c163b3763bf8703148a
KevinBett <kevinbett458@gmail.com>
2023-05-05 10:19:29 +0300

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Favorites from './components/Favorites';
-import "./App.css"
+// import Favorites from './components/Favorites';
+import "./App.css";
 import Navbar from './components/Navbar';
-import { FavoritesContext } from './components/FavoritesContext';
 import About from './components/About';
 import RecipeForm from './components/RecipeForm';
 
 function App() {
-  const [favorites, setFavorites] = useState([]);
+  // const [favorites, setFavorites] = useState([]);
   
 
-  const addTofavorites = (meal) => {
-    setFavorites((prevFavorites) => [...prevFavorites, meal]);
-  };
+  // const addTofavorites = (meal) => {
+  //   setFavorites((prevFavorites) => [...prevFavorites, meal]);
+  // };
 
   return (
     <>
@@ -22,7 +21,7 @@ function App() {
 
       <Routes>
         <Route exact path='/' element={<Home />} />
-        <Route path='/favorites' element={<favorites favorites={favorites} />} />
+        <Route path='/favorites' element={<favorites />} />
         <Route path='/about' element={<About />} />
         <Route path='/recipeform' element={<RecipeForm />} />
       </Routes>
